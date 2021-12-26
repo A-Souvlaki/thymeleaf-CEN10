271cd5472f0c5eba95c273bf56165a1590b9a08f
A-Souvlaki <josepthcamilo@gmail.com>
2021-12-26 14:34:05 -0500

    initial commit

---
+++ src/main/java/co/edu/icesi/ci/thymeval/service/UserServiceImpl.java
@@ -0,0 +1,93 @@
+package co.edu.icesi.ci.thymeval.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.icesi.ci.thymeval.model.User;
+import co.edu.icesi.ci.thymeval.model.UserType;
+import co.edu.icesi.ci.thymeval.model.UserGender;
+import co.edu.icesi.ci.thymeval.repository.UserRepository;
+import co.edu.icesi.ci.thymeval.service.interfaces.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+	private UserRepository userRepository;
+
+	@Autowired
+	public UserServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	public User save(User user) {
+		return userRepository.save(user);
+
+	}
+
+	public Optional<User> findById(long id) {
+
+		return userRepository.findById(id);
+	}
+
+	public Iterable<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	public Iterable<User> findAllPatients() {
+		return userRepository.findByType(UserType.patient);
+	}
+	
+	public Iterable<User> findAllDoctors() {
+		return userRepository.findByType(UserType.doctor);
+	}
+
+
+	public void delete(User user) {
+		userRepository.delete(user);
+
+	}
+
+	public UserGender[] getGenders() {
+		
+		return UserGender.values();
+	}
+
+	public UserType[] getTypes() {
+		// TODO Auto-generated method stub
+		return UserType.values();
+	}
+	
+	public void updateUserForSave(User user) {
+		User modUser = userRepository.findById(user.getId()).get();
+		//modUser.setBirthDate(user.getBirthDate());
+		modUser.setEmail(user.getEmail());
+		modUser.setGender(user.getGender());
+		modUser.setName(user.getName());
+		modUser.setType(user.getType());
+		
+		//modUser.setUsername(user.getUsername());
+		
+		/*if(user.getPassword() != null) {
+			modUser.setPassword(user.getPassword());
+		}*/
+		userRepository.save(modUser);
+	}
+	
+	public void update(User user) {
+		User modUser = userRepository.findById(user.getId()).get();
+		modUser.setBirthDate(user.getBirthDate());
+		modUser.setEmail(user.getEmail());
+		modUser.setGender(user.getGender());
+		modUser.setName(user.getName());
+		modUser.setType(user.getType());
+		//modUser.setUsername(user.getUsername());
+		
+		if(!user.getPassword().equals("")) {
+			modUser.setPassword(user.getPassword());
+		}
+		userRepository.save(modUser);
+	}
+}
